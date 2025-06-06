c702b6d485c22b7108355072ab3e7a0b880c2547
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-06-06 23:25:46 +0700

    Menambahkan method login di auth_repository.

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:rest_api/data/models/request/auth/login_request_model.dart';
 import 'package:rest_api/services/services_http_client.dart';
 
 class AuthRepository {
@@ -8,4 +9,6 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._servicesHttpClient);
+
+  Future login(LoginRequestModel requestModel) async {}
 }
