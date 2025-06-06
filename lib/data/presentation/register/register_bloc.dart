31cbee119e437e6148cc295f1714634edb5b3566
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-06-06 23:33:16 +0700

    Menambahkan RegisterBloc dengan event dan state untuk menangani bussines logic dari proses registrasi.

---
+++ lib/data/presentation/register/register_bloc.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import 'package:rest_api/data/models/request/auth/register_request_model.dart';
+import 'package:rest_api/data/repository/auth_repository.dart';
+
+part 'register_event.dart';
+part 'register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository authRepository;
+
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(message: r)),
+    );
+  }
+}
