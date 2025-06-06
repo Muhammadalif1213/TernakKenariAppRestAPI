13b06ae2181508f9ea6230b5e450bfe709eb93dc
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-06-06 23:32:22 +0700

    Menambahkan method register di auth_repository.

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:rest_api/data/models/request/auth/login_request_model.dart';
+import 'package:rest_api/data/models/request/auth/register_request_model.dart';
 import 'package:rest_api/services/services_http_client.dart';
 
 class AuthRepository {
@@ -11,4 +12,6 @@ class AuthRepository {
   AuthRepository(this._servicesHttpClient);
 
   Future login(LoginRequestModel requestModel) async {}
+
+  Future register(RegisterRequestModel requestModel) async {}
 }
